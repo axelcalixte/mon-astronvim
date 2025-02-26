98ffa2f2f70fba27ce6d3c96075a142bf8aa4dbb
Axel Calixte <axelcalixte@mailbox.org>
2025-02-26 21:48:09 +0100

    merge my configs into astronvim v4

--- lua/community.lua
+++ lua/community.lua
@@ -1,12 +1,10 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- AstroCommunity: import any community modules here
--- We import this file in `lazy_setup.lua` before the `plugins/` folder.
--- This guarantees that the specs are processed before any user plugins.
-
 ---@type LazySpec
 return {
-  "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+   "AstroNvim/astrocommunity",
+   { import = "astrocommunity.pack.lua" },
+   { import = "astrocommunity.pack.typescript-deno" },
+   { import = "astrocommunity/syntax/vim-sandwich" },
+   { import = "astrocommunity.editing-support.undotree" },
+   { import = "astrocommunity/completion/cmp-git" },
+   { import = "astrocommunity.editing-support.nvim-treesitter-context" },
 }
